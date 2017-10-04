3729c435c25cad25c7fc9c30cf2bb1b0f5a76ab8
Marius <marius.shiman@gmail.com>
2017-10-04 18:19:18 +0300

    fix

--- src/HW/Main.java
+++ src/HW/Main.java
@@ -28,9 +28,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("HW");
-
 		initRootLayout();
-
 		showHWOverview();
 	}
 
